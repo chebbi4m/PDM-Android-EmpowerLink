32298f66bd5bc1641456d324dc21aad2d561b2ff
mohamed chebbi <chebbim4@gmail.com>
2023-11-09 05:47:46 +0100

    addded community

---
+++ app/src/main/java/tn/esprit/pdm/community.kt
@@ -0,0 +1,8 @@
+package tn.esprit.pdm
+
+data class Community(
+    var communityImage: Int, // Changed data type to Int
+    var communityTitle: String,
+    var communityCategory: String,
+    var communityObjectif: String
+)
