52ba7a7f4e83417df43990e04cd107ec38142d05
wassimnsiri <wassim.nsiri@esprit.tn>
2023-11-12 12:46:54 +0100

    fix

--- app/src/main/java/tn/esprit/pdm/models/Community.kt
+++
@@ -1,9 +0,0 @@
-package tn.esprit.pdm.models
-
-data class Community(
-    var id: Long,
-    var communityImage: Int,
-    var communityTitle: String,
-    var communityCategory: String,
-    var communityObjectif: String
-)
